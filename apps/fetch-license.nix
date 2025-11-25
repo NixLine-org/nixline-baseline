fb7a455c9820e6e2d41d78e81483387b4dc74bf8
Jason Odoom <jasonodoom@riseup.net>
2025-11-24 21:11:37 -0500

    Fix fetch-license app name from nixline to lineage

--- apps/fetch-license.nix
+++ apps/fetch-license.nix
@@ -21,7 +21,7 @@
 */
 
 pkgs.writeShellApplication {
-  name = "nixline-fetch-license";
+  name = "lineage-fetch-license";
 
   runtimeInputs = [ pkgs.curl pkgs.jq ];
 
@@ -37,8 +37,8 @@ pkgs.writeShellApplication {
 Fetch license text from SPDX and generate license pack.
 
 Usage:
-  nixline-fetch-license <license-id> --year YYYY [--holder "Name"]
-  nixline-fetch-license --list
+  lineage-fetch-license <license-id> --year YYYY [--holder "Name"]
+  lineage-fetch-license --list
 
 Options:
   <license-id>     SPDX license identifier (e.g., Apache-2.0, MIT, GPL-3.0)
@@ -48,13 +48,13 @@ Options:
 
 Examples:
   # Fetch Apache 2.0 license
-  nixline-fetch-license Apache-2.0 --holder "My Company" --year 2025
+  lineage-fetch-license Apache-2.0 --holder "My Company" --year 2025
 
   # Fetch MIT license
-  nixline-fetch-license MIT --holder "ACME Corp"
+  lineage-fetch-license MIT --holder "ACME Corp" --year 2025
 
   # List common licenses
-  nixline-fetch-license --list
+  lineage-fetch-license --list
 
 Output:
   Generated pack file is written to packs/license.nix
@@ -119,7 +119,7 @@ LIST_EOF
 
     if [[ -z "$COPYRIGHT_YEAR" ]]; then
       echo "Error: --year is required" >&2
-      echo "Example: nixline-fetch-license Apache-2.0 --holder \"ACME Corp\" --year 2025" >&2
+      echo "Example: lineage-fetch-license Apache-2.0 --holder \"ACME Corp\" --year 2025" >&2
       exit 1
     fi
 
@@ -157,7 +157,7 @@ LIST_EOF
 #
 PACK_HEADER
 
-      echo "# Generated using: nixline-fetch-license $LICENSE_ID"
+      echo "# Generated using: lineage-fetch-license $LICENSE_ID"
       echo "# License: $LICENSE_ID"
       echo "# Copyright Holder: $COPYRIGHT_HOLDER"
       echo "# Copyright Year: $COPYRIGHT_YEAR"
